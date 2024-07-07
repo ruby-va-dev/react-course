8cf1eae97ade1939f6aec7e0a7c372018be804ba
ruby-va-dev <rubyva2000@gmail.com>
2024-07-07 19:46:17 +0800

    feat: add eslint and prettier to project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,31 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-refresh',
+    'react-compiler',
+    'prettier',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return <h1>HAllo!</h1>;
+  return <h1>HAllo!</h1>
 }
 
-export default App;
+export default App
