aaa36e92a0fec327d7e4140e98fd0dd21bf818d1
ruby-va-dev <rubyva2000@gmail.com>
2024-07-08 03:50:35 +0800

    init: init project

--- src/const/character-genders.ts
+++
@@ -1,8 +0,0 @@
-export const charactersGenders = {
-  Male: 'Мужской',
-  Female: 'Женский',
-  Genderless: 'Бесполый',
-  unknown: 'Неизвестный',
-} as const
-
-export type CharacterGenderKeys = keyof typeof charactersGenders
